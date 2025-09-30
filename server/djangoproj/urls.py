d53e1be1643fc1d8d73b938023dcb2156bdfedf1
JJ <jordanlangworthy@gmail.com>
2025-09-30 11:19:52 -0400

    Save progress: dealers page + env + build

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,7 +1,6 @@
 """djangoproj URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+The `urlpatterns` list routes URLs to views.
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -10,11 +9,18 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # App (Django) APIs
     path('djangoapp/', include('djangoapp.urls')),
+
+    # Static template pages (already provided in starter)
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+
+    # React routes (served by index.html; React Router handles the view)
     path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
